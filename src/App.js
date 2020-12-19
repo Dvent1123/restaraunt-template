7eab9a56ce3de86fc84e6cfe72f9121862b62b10
Dvent1123 <58802270+Dvent1123@users.noreply.github.com>
2020-12-18 17:31:38 -0700

    Styled Map Container

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react'
-// import NavBar from './NavBar'
-// import Banner from './Banner'
-// import FullMenu from './FullMenu'
-// import {Review} from './Review'
+import NavBar from './NavBar'
+import Banner from './Banner'
+import FullMenu from './FullMenu'
+import {Review} from './Review'
 import Maps from './Maps'
 function App() {
   return (
     <>
-      {/* <NavBar /> 
+      <NavBar /> 
       <Banner />
       <FullMenu />
-      <Review /> */}
+      <Review />
       <Maps/>
     </>
   )
--- src/Maps.js
+++ src/Maps.js
@@ -1,20 +1,34 @@
 import React from 'react'
-import {Map, GoogleApiWrapper} from 'google-maps-react'
+import {Map, GoogleApiWrapper, Marker} from 'google-maps-react'
 
 export class Maps extends React.Component{
     render(){
         const mapStyles = {
-            width: '100%',
-            height: '100%'
+            width: '80vw',
+            height: '75%',
+            position: 'relative'
+        }
+
+        const mapContainer = {
+            display: 'flex',
+            padding: '10px',
+            justifyContent: 'center',
+            alignItems: 'center'
+        
         }
         
-        return (
-        <Map
-            google={this.props.google}
-            zoom={15}
-            style={mapStyles}
-            initialCenter={{lat: 9.761927, lng: 79.95244}}
-        />
+        return (<div className="map">
+            <h2 className="map-title">Located</h2>
+                    <Map
+                        google={this.props.google}
+                        zoom={15}
+                        style={mapStyles}
+                        containerStyle={mapContainer}
+                        initialCenter={{lat: 9.761927, lng: 79.95244}}
+                    > 
+                        <Marker position={{lat: 9.761927, lng: 79.95244}}/>
+                    </ Map>
+        </div>
         )
     }
 
