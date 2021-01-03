6617bb916bd3defe3f7b89ccaac689cf56182450
Dvent1123 <58802270+Dvent1123@users.noreply.github.com>
2021-01-02 20:35:03 -0700

    More Mobile Styling

--- src/components/About.js
+++ src/components/About.js
@@ -3,24 +3,29 @@ import outside from '../assets/images/outside-rest-pic.jpg'
 
 const About = () => {
     return (
+        <main className='about-main'>
         <section className='about-container' id='about'>
             <div className='about-title-container'>
                 <h2 className='about-title'>About</h2>
             </div>
             <div className="about-content">
-                <h3 className="about-text">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                    Error quasi laboriosam nemo, nisi similique nam quam asperiores ullam nesciunt repellendus? 
-                    Autem ratione nulla eos suscipit, obcaecati sapiente et officiis eaque.
-                    <br/>
-                    <br/>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                    Est, commodi qui blanditiis, placeat vel porro dolorem at excepturi 
-                    impedit perferendis odit ducimus unde magni libero dolor eum ratione inventore! Labore!
-                </h3>
+                <div className="about-text-container">
+                    <h3 className="about-text">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                        Error quasi laboriosam nemo, nisi similique nam quam asperiores ullam nesciunt repellendus? 
+                        Autem ratione nulla eos suscipit, obcaecati sapiente et officiis eaque.
+                        <br/>
+                        <br/>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                        Est, commodi qui blanditiis, placeat vel porro dolorem at excepturi 
+                        impedit perferendis odit ducimus unde magni libero dolor eum ratione inventore! Labore!
+                    </h3>
+                </div>
                 <img className='about-img' src={outside} alt="restaurant"/>
             </div>
 
         </section>
+        </main>
+
     )
 }
 
