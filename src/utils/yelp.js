2f6dd35d2bef639fd5d00af34a821adb42b25d92
Dvent1123 <58802270+Dvent1123@users.noreply.github.com>
2021-03-04 12:32:05 -0700

    staging amplify

--- src/utils/yelp.js
+++ src/utils/yelp.js
@@ -3,6 +3,7 @@ import aws_exports from "../aws-exports";
 Amplify.configure(aws_exports);
 const apiName = 'restarauntapi'
 const path = 'https://k559qk1314.execute-api.us-east-2.amazonaws.com/dev'
+//just for redeploy
 
 export const getReviews = async () => {
     let res = await API.get(apiName, path)
