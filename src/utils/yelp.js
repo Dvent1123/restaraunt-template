7fcef0ea9e008969e76a39d477b251719150de2b
Dvent1123 <58802270+Dvent1123@users.noreply.github.com>
2021-03-04 13:24:10 -0700

    no path for amplify

--- src/utils/yelp.js
+++ src/utils/yelp.js
@@ -2,11 +2,10 @@ import Amplify, { API } from "aws-amplify";
 import aws_exports from "../aws-exports";
 Amplify.configure(aws_exports);
 const apiName = 'restarauntapi'
-const path = '/'
 //just for redeploy
 
 export const getReviews = async () => {
-    let res = await API.get(apiName, path)
+    let res = await API.get(apiName)
 
     return res.data || []
 }
